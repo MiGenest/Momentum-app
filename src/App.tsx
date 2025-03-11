f4d4d4d9b550b85ec8ff71126c10aa87e223ce71
MiGenest <Mishgenest@gmail.com>
2025-03-11 22:26:54 +0400

    Initialize project structure with basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route,} from 'react-router-dom';
 import './App.css';
+
+import Dashboard from '../pages/Dashboard';
+import TaskCreate from '../pages/TaskCreate';
+
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={Dashboard} />
+        <Route path="/create" element={<TaskCreate />} />
+        <Route path="/edit/:id" element={<TaskCreate />} />
+      </Routes>
+    </Router>
   );
 }
 
